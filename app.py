b1fb6bc5d824f8b43ea74410af4491853418bbf9
FA786786 <mohammadimran1389@gmail.com>
2025-04-25 11:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import os
+# app.py
 
-from auth import get_google_sheet
 import os
 import streamlit as st
 
-from auth import get_google_sheet, get_keys_from_sheet, generate_login_url, exchange_token, update_access_token
+from auth import (
+    get_google_sheet,
+    get_keys_from_sheet,
+    generate_login_url,
+    exchange_token,
+    update_access_token,
+)
 
-st.title("Test App")
-from auth import get_google_sheet
+st.title("Streamlit App")
+st.write("Hello world")
