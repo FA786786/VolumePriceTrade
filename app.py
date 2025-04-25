a95d4d726e0a4bb8e9283fdabda5bb6758d202b6
FA786786 <mohammadimran1389@gmail.com>
2025-04-25 10:58:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ import streamlit as st
 from auth import get_google_sheet, get_keys_from_sheet, generate_login_url, exchange_token, update_access_token
 
 st.title("Test App")
+from auth import get_google_sheet
