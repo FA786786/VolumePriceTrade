4aa2136b9f6aedadad2ec9578e6966cf8ecf04ad
THE FINANCIAL ASSETS <mohammadimran1389@gmail.com>
2025-05-01 10:57:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ from auth import get_google_sheet
 json_key = st.secrets["google_sheets"]
 sheet_url = "https://docs.google.com/spreadsheets/d/your_sheet_id_here"
 sheet = get_google_sheet(sheet_url, json_key)
+st.write("✅ Secret loaded:", st.secrets["google_sheets"]["client_email"])
